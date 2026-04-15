fca9ae1e309926464a211b3983b527631e9a7dcd
Punaji-Zore <zorepunaji@gmail.com>
2026-04-15 14:15:01 +0530

    Solved Rotate Array (LeetCode 189) using reverse approach

---
+++ src/dsa/arrays/RotateArray_LeetCode_189.java
@@ -0,0 +1,33 @@
+package dsa.arrays;
+/*
+Problem: Rotate Array (LeetCode 189)
+
+Given an array, rotate it to the right by k steps.
+
+Example:
+Input: [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+*/
+public class RotateArray_LeetCode_189 {
+    public static void reverse(int[] arr, int i,int j){
+        while (i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int k = 3;
+        k = k%n;
+        reverse(arr, 0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+        for(int ele : arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
